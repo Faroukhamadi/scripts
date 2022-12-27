07ce85e6fe0d2c867003bfe576d1ebfc9d386e36
faroukhamadi <faroukhamadi1@outlook.com>
2022-12-27 06:28:28 +0100

    modify the script so that only one arg is required

--- initialize_git/init_git.py
+++ initialize_git/init_git.py
@@ -3,14 +3,17 @@ from sys import argv
 
 
 def init_git():
+    # get repository name from folder
+    repo_name = argv[1].split('/')[-1]
+
     print(f"Initializing new git repository in {argv[1]}")
-    os.system(f"echo \"# {argv[1]}\" >> README.md")
+    os.system(f"echo \"# {repo_name}\" >> README.md")
     os.system("git init")
-    os.system(f"git add {argv[2]}")
+    os.system(f"git add {argv[1]}")
     os.system("git commit -m \"first commit\"")
     os.system("git branch -M main")
     os.system(
-        f"git remote add origin git@github.com:Faroukhamadi/{argv[1]}.git"
+        f"git remote add origin git@github.com:Faroukhamadi/{repo_name}.git"
     )
     os.system("git push -u origin main")
 
